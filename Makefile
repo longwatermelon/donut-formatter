84d000224719f70fba7471a4c0529adea7a93973
longwatermelon <longwaddermelon@gmail.com>
2021-12-10 17:23:23 -0700

    donut

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 CC=gcc
 CFLAGS=-std=gnu17 -ggdb -Wall -Werror
+LIBS=-lm
 
 formatter: main.o
-	$(CC) $(CFLAGS) $< -o $@
+	$(CC) $(CFLAGS) $(LIBS) $< -o $@
 
 main.o: main.c
-	$(CC) $(CFLAGS) -c $< -o $@
+	$(CC) $(CFLAGS) $(LIBS) -c $< -o $@
 
 clean:
 	rm *.o
--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 char* read_file(const char* path)
 {
@@ -34,6 +37,40 @@ char* read_file(const char* path)
     return contents;
 }
 
+char** generate_donut(char* contents, int radius, int inner_radius, size_t* nlines)
+{
+    int width = radius * 4;
+    int height = radius * 2 + 1;
+
+    char** lines = malloc(sizeof(char*) * height);
+    *nlines = height;
+
+    size_t curr_idx = 0;
+
+    for (int y = 0; y < height; ++y)
+    {
+        lines[y] = malloc(sizeof(char) * (width + 1));
+        memset(lines[y], ' ', sizeof(char) * width);
+        lines[y][width] = '\0';
+
+        for (int x = 0; x < width; ++x)
+        {
+            int dx = ((width / 2) - x) / 2;
+            int dy = (height / 2) - y;
+
+            int dist = sqrt(dx * dx + dy * dy);
+
+            if (dist < radius && dist > inner_radius)
+            {
+                if (curr_idx + 1 < strlen(contents))
+                    lines[y][x] = contents[curr_idx++];
+            }
+        }
+    }
+
+    return lines;
+}
+
 int main(int argc, char** argv)
 {
     if (argc == 1)
@@ -43,6 +80,18 @@ int main(int argc, char** argv)
     }
 
     char* contents = read_file(argv[1]);
+
+    size_t nlines;
+    char** lines = generate_donut(contents, 20, 5, &nlines);
+
+    for (size_t i = 0; i < nlines; ++i)
+    {
+        printf("%s\n", lines[i]);
+        free(lines[i]);
+    }
+
+    free(contents);
+    free(lines);
 
     return 0;
 }
